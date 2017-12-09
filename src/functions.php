6acd465d9f72394217918823f34453804a536257
vinkim <vinkim@xiyanghui.com>
2017-12-09 11:04:28 +0800

    初始化

---
+++ src/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinkim
+ * Date: 09/12/2017
+ * Time: 10:49 AM
+ */
+
+namespace Ryum\ExtFunc;
+
+function test()
+{
+    echo 'test';
+}
---
+++ tests/test-.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinkim
+ * Date: 09/12/2017
+ * Time: 10:43 AM
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+\Ryum\ExtFunc\test();
